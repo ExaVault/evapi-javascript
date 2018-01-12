c02065bf632282e71f5a7563224ec61e4ba298e9
Dima <dima.procentr@gmail.com>
2018-01-12 23:59:58 +0200

    Add ready to use example snippet file

---
+++ example_snippet.js
@@ -0,0 +1,98 @@
+var ExaVaultApi = require('exa_vault_api');
+
+var authenticationApi = new ExaVaultApi.AuthenticationApi();
+
+var apiKey = 'ftptest-0KM31a1OLrR2N42OrC9l9'; 
+var username = 'ftptest';
+var password = 'quaker17'; 
+
+var opts = { 
+  'username': username,
+  'password': password
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    // server error occured
+    console.error(error);
+  } else {
+    var loginSuccess = data.success;
+
+    if (loginSuccess) {
+      var accessToken = data.results.accessToken;
+    } else {
+      // something went wrong check data.error for more details
+      console.error(data.error);
+      return;
+    }
+
+    var filesFoldersApi = new ExaVaultApi.FilesAndFoldersApi();
+
+    var folderName = 'api_test_folder' + Math.floor(Math.random() * 10000);
+    var path = '/';
+
+    var opts = { 
+      'access_token': accessToken,
+      'folderName': folderName,
+      'path': path
+    };
+
+    var callback = function(error, data, response) {
+      if (error) {
+        // server error occured
+        console.error(error);
+      } else {
+
+        var createSuccess = data.success;
+
+        if (createSuccess) {
+          // Folder created successfully
+          console.log('Folder created successfully');
+
+          var activityApi = new ExaVaultApi.ActivityApi();
+
+          var opts = { 
+            'offset': 0,
+            'sort_by': 'sort_logs_date',
+            'sort_order': 'desc'
+          };
+
+
+        var callback = function(error, data, response) {
+          if (error) {
+            //server error occured
+            console.error(error);
+          } else {
+
+            var getActivitySuccess = data.success;
+
+            if(getActivitySuccess) {
+              // Geat array with log entries
+              var activityLogs = data.results;
+              console.log(activityLogs);
+             } else {
+              // something went wrong check data.error for more details
+              console.error(data.error);
+              return;
+            }
+            
+          }
+        };
+
+        activityApi.getFileActivityLogs(apiKey, accessToken, opts, callback);
+
+        } else {
+          // something went wrong check data.error for more details
+          console.error(data.error);
+          return;
+        }
+
+      }
+    };
+
+    filesFoldersApi.createFolder(apiKey, opts, callback);
+
+  }
+};
+
+authenticationApi.authenticateUser(apiKey, opts, callback);
